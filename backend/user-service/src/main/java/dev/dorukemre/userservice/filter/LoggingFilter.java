48ae9bae6a2ecb437cdae39b1cf7a7d825f13fe1
Doruk Emre <doruk.emre@outlook.com>
2026-01-21 16:14:24 +0100

    Add log on all incoming requests to user-service

---
+++ backend/user-service/src/main/java/dev/dorukemre/userservice/filter/LoggingFilter.java
@@ -0,0 +1,27 @@
+package dev.dorukemre.userservice.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request,
+      ServletResponse response,
+      FilterChain chain)
+      throws IOException, ServletException {
+
+    HttpServletRequest req = (HttpServletRequest) request;
+    System.out.println("Request: " + req.getMethod() + " " + req.getRequestURI());
+
+    chain.doFilter(request, response);
+  }
+}
