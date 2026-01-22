9c89e7ed18a2fea7cbdf990fa292febaa94f021a
Doruk Emre <doruk.emre@outlook.com>
2026-01-22 17:05:18 +0100

    Use react-bootstrap in Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import axios from 'axios';
 import { addBaseUrl } from '@/utils/globals';
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Container } from 'react-bootstrap';
+
 const Login = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -45,37 +49,53 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <Container fluid className="d-flex flex-column align-items-center gap-2" style={{ maxWidth: '600px' }}>
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Username:
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password:
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      <div>
+        <p className="text-muted">
+          To log in as a standard user:
+          Username: standard
+          Password: Standard1
+        </p>
+        <p className="text-muted">
+          To log in as a service desk user:
+          Username: service
+          Password: Service1
+        </p>
+        <p className="text-muted">
+          To log in as an admin:
+          Username: admin
+          Password: Admin1
+        </p>
+      </div>
+
+      <Form onSubmit={handleSubmit} className="w-100" style={{ maxWidth: '366px' }}>
+        <Form.Group className="mb-3" controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Log in
+        </Button>
+      </Form>
+
+    </Container>
   );
 };
 
