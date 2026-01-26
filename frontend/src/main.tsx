5a8b0a8cd9a143e5089bc9647185d6c6d7210d75
Doruk Emre <doruk.emre@outlook.com>
2026-01-26 22:32:50 +0100

    Update colour theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "@/scss/_custom.scss";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import "@/css/custom.scss";
-import '@/css/reset.css'
-import '@/css/index.css'
+import "@/css/reset.css";
+import '@/css/index.css';
+
 
 import App from '@/App.tsx'
 import queryClient from '@/config/queryClient';
