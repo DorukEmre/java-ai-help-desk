6f80e8eb0067e59e1e52372b6b1e9514736ee51f
Doruk Emre <doruk.emre@outlook.com>
2026-01-29 13:55:20 +0100

    Refactor ErrorMessage component to handle unknown error types

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -1,16 +1,33 @@
+import axios from "axios";
 import { useRef, useEffect } from "react";
 
-export const ErrorMessage = ({ error }: { error: string | null }) => {
+export const ErrorMessage = ({ error }: { error: unknown }) => {
 
   const errorRef = useRef<HTMLParagraphElement>(null);
 
+  // Extract error message
+  const message: string | null = (() => {
+    if (!error)
+      return null;
+    if (axios.isAxiosError(error))
+      return (error.response?.data?.message as string) ?? error.message;
+    if (error instanceof Error)
+      return error.message;
+
+    return String(error);
+  })();
+
+  // Scroll page to error message
   useEffect(() => {
-    if (error && errorRef.current) {
+    if (message && errorRef.current) {
       const element = errorRef.current.getBoundingClientRect().top + window.scrollY;
       const adjustedScrollPosition = element - 90;
       window.scrollTo({ top: adjustedScrollPosition, behavior: 'smooth' });
     }
-  }, [error]);
+  }, [message]);
+
+  if (!message)
+    return null;
 
   return (
     <>
@@ -20,7 +37,7 @@ export const ErrorMessage = ({ error }: { error: string | null }) => {
           className="bg-danger px-3 py-2 m-2 rounded"
           style={{ width: "max-content" }}
         >
-          {error}
+          {message}
         </p>
       )}
     </>
