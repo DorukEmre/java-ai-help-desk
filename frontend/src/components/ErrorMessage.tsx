4febb27a5bcb56e9cbad65808e9f496eac925d38
Doruk Emre <doruk.emre@outlook.com>
2026-01-29 11:48:22 +0100

    Add scrolling to error message on error

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -1,9 +1,27 @@
+import { useRef, useEffect } from "react";
+
 export const ErrorMessage = ({ error }: { error: string | null }) => {
+
+  const errorRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (error && errorRef.current) {
+      const element = errorRef.current.getBoundingClientRect().top + window.scrollY;
+      const adjustedScrollPosition = element - 90;
+      window.scrollTo({ top: adjustedScrollPosition, behavior: 'smooth' });
+    }
+  }, [error]);
 
   return (
     <>
       {error && (
-        <p className="bg-danger px-3 py-2 m-2 rounded" style={{ width: "max-content" }}>{error}</p>
+        <p
+          ref={errorRef}
+          className="bg-danger px-3 py-2 m-2 rounded"
+          style={{ width: "max-content" }}
+        >
+          {error}
+        </p>
       )}
     </>
   )
