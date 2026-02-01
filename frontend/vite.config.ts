e9da458736202da0e3bd671feb1aafcf9b63d027
Doruk Emre <doruk.emre@outlook.com>
2026-02-01 23:31:04 +0100

    Configure vite to use https in development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import fs from "fs"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['color-functions', 'global-builtin', 'import', 'if-function']
-      },
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      https: isDev
+        ? {
+          key: fs.readFileSync("./localhost-key.pem"),
+          cert: fs.readFileSync("./localhost-cert.pem"),
+        }
+        : undefined,
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['color-functions', 'global-builtin', 'import', 'if-function']
+        },
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  }
 })
