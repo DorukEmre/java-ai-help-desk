45095aad1466f34d6092bbcf91f0ddbb0dd70b09
Doruk Emre <doruk.emre@outlook.com>
2026-02-02 19:11:09 +0100

    Remove data in localStorage and add refresh request on app load

--- frontend/src/auth/AuthProvider.tsx
+++ frontend/src/auth/AuthProvider.tsx
@@ -1,39 +1,56 @@
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import { AuthContext, type AuthContextValue } from "@/auth/AuthContext";
 import type { User, LoginResponse } from "@/types/auth";
+import { getBaseUrl } from "@/utils/globals";
 
-
-const readAuthFromStorage = (): { accessToken: string | null; user: User | null } => {
-  const accessToken = localStorage.getItem("accessToken");
-  const userRaw = localStorage.getItem("user");
-
-  // If one missing, clean up
-  if (!accessToken || !userRaw) {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("user");
-
-    return { accessToken: null, user: null };
-  }
-
-  try {
-    const user = JSON.parse(userRaw) as User;
-    return { accessToken, user };
-  } catch {
-    // If JSON parse fail, clean up
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("user");
-
-    return { accessToken: null, user: null };
-  }
-};
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const navigate = useNavigate();
 
-  const [{ accessToken, user }, setAuth] = useState(readAuthFromStorage);
+  const [{ accessToken, user }, setAuth] = useState<{
+    accessToken: string | null;
+    user: User | null;
+  }>({
+    accessToken: null,
+    user: null
+  });
+
+  // Refresh request on app load
+  useEffect(() => {
+    const refresh = async () => {
+      try {
+
+        const url = getBaseUrl() + "/refresh";
+        const response = await axios.post(
+          url,
+          {},
+          { withCredentials: true, }
+        );
+
+        const data: LoginResponse = response.data;
+        console.debug("AuthProvider refresh: ", data)
+
+        setAuth({
+          accessToken: data.accessToken,
+          user: {
+            id: data.id,
+            username: data.username,
+            fullname: data.fullname,
+            role: data.role
+          }
+        });
+      } catch {
+        setAuth({ accessToken: null, user: null });
+
+      }
+    };
+
+    refresh();
+  }, []);
 
   // On user log in
   const setAuthSession = ({ accessToken, ...userData }: LoginResponse) => {
