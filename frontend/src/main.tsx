6db7bf16859040395a7b50c1841277857d2f7272
Doruk Emre <doruk.emre@outlook.com>
2026-02-06 16:38:34 +0100

    Refactor CSS and SCSS files to split into variables, bootstrap, and components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import "@/scss/_custom.scss";
+import "@/scss/main.scss";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import "@/css/reset.css";
 import '@/css/index.css';
 
 
